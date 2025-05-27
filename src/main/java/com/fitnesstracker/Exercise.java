687435860979681e52d6439021f27cfd7d42a132
Noah Whiffen <Noah.Whiffen@keyin.com>
2025-05-27 15:37:08 -0230

    added an exercise class

---
+++ src/main/java/com/fitnesstracker/Exercise.java
@@ -0,0 +1,33 @@
+package main.java.com.fitnesstracker;
+
+public class Exercise {
+    private String name;
+    private int reps;
+    private int sets;
+}
+
+// Getters
+public String getName() {
+    return name;
+}
+
+public int getReps() {
+    return reps;
+}
+
+public int getSets() {
+    return sets;
+}
+
+// Setters
+public void setName(String name) {
+    this.name = name;
+}
+
+public void setReps(int reps) {
+    this.reps = reps;
+}
+
+public void setSets(int sets) {
+    this.sets = sets;
+}
